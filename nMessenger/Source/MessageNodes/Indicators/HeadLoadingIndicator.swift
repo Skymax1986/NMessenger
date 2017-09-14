e13e88c6c1228f09b0945d5abd5244baa77bd38a
Markus Kerschbaum <markus.kerschbaum@rise-world.com>
2017-09-14 17:18:35 +0200

    fixes viewblock-setter

--- nMessenger/Source/MessageNodes/Indicators/HeadLoadingIndicator.swift
+++ nMessenger/Source/MessageNodes/Indicators/HeadLoadingIndicator.swift
@@ -71,9 +71,8 @@ open class SpinnerNode: ASDisplayNode {
     }
 
     public override init() {
-        super.init()
-        self.setViewBlock({ UIActivityIndicatorView(activityIndicatorStyle: .gray) })
-//        super.init(viewBlock: { UIActivityIndicatorView(activityIndicatorStyle: .gray) }, didLoad: nil)
+       // self.setViewBlock({ UIActivityIndicatorView(activityIndicatorStyle: .gray) })
+        super.init(viewBlock: { UIActivityIndicatorView(activityIndicatorStyle: .gray) }, didLoad: nil)
         self.style.preferredSize.height = 32
     }
     
